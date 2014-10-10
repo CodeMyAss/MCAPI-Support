bd7a025536ad04679071f430d85e377d92972e8c
Yive <admin@yive.me>
2014-10-10 10:10:19 -0700

    Added Perl examples

---
+++ examples/Yive/ServerStatus.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use HTML::Template;
+use LWP::Simple qw(get);
+use JSON qw( decode_json );
+my $decoded = decode_json(get("http://mcapi.ca/v2/query/info/?ip=us.mineplex.com"));
+print qq(Content-type: text/html\n\n);
+
+# Status: $info->{'status'}
+# MOTD: $info->{'motd'}
+# Version: $info->{'version'}
+# Players Online: $info->{'players'}{'online'}
+# Players Max: $info->{'players'}{'max'}
+# Ping: $info->{'ping'}
+
+# This will not display the player names who are online, you'll need to use the list API.
+if(!$decoded->{'status'}) {
+	print 'The server is offline';
+} else {
+	print 'The server is online with '.$decoded->{'players'}{'online'}.'/'.$decoded->{'players'}{'max'}.' players online.'
+}
